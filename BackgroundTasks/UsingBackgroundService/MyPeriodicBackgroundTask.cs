246e2d4a2bba63917eed93b7725ce82f4b40373f
Hadi Rezaee <se.hadirezaee@gmail.com>
2022-12-04 00:04:02 +0100

    add peridic task

---
+++ BackgroundTasks/UsingBackgroundService/MyPeriodicBackgroundTask.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace BackgroundTasks.UsingBackgroundService;
+
+public class MyPeriodicBackgroundTask : BackgroundService
+{
+    private readonly TimeSpan _period = TimeSpan.FromMinutes(10);
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using PeriodicTimer timer = new PeriodicTimer(_period);
+
+        while (!stoppingToken.IsCancellationRequested &&
+               await timer.WaitForNextTickAsync(stoppingToken))
+        {
+            //process here
+        }
+
+        return;
+    }
+}

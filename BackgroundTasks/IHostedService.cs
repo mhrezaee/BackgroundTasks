8f73c8d4fc6ee78743f08f52544745f60b48bb99
Hadi Rezaee <se.hadirezaee@gmail.com>
2022-12-03 23:13:11 +0100

    Add project files.

---
+++ BackgroundTasks/IHostedService.cs
@@ -0,0 +1,8 @@
+﻿namespace BackgroundTasks;
+
+public interface IHostedService
+{
+    Task StartAsync(CancellationToken cancellationToken);
+
+    Task StopAsync(CancellationToken cancellationToken);
+}
